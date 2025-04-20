b19c6211febb0353738539e0041ea7ae65c52dc1
Yiff-Me-Daddy <keirookami@gmail.com>
2025-04-20 03:02:53 -0700

    Initial commit

---
+++ src/main/java/com/donnybear/miningdimension/MiningDimension.java
@@ -0,0 +1,10 @@
+package com.donnybear.miningdimension;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("miningdimension")
+public class MiningDimension {
+    public MiningDimension() {
+        System.out.println("Mining Dimension mod loaded by Donny_Bear!");
+    }
+}
